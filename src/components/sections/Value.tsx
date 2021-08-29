9d96ddff63c9a2ddbcb392527a110318987a9ec4
u-Hoshi <yuta000707@yahoo.co.jp>
2021-08-29 22:10:45 +0900

    futureの作成(レスポンシブ対応済み)

--- src/components/sections/Value.tsx
+++ src/components/sections/Value.tsx
@@ -5,27 +5,29 @@ import SectionTitle from "../datadisplay/SectionTitle"
 const Value = () => {
   return (
     <>
-      <div className='text-center'>
+      <div className=' relative z-10 text-center'>
         <SectionTitle title='価値観' subtitle='VALUES' />
-        <div className='sm:flex sm:justify-center'>
-          <div className='px-10 pt-7 sm:pb-7 mx-auto sm:mx-0 sm:w-96 max-w-md'>
+        <div className='lg:flex lg:justify-center lg:mx-32 sm:bg-white'>
+          <div className=' px-4 lg:px-10 lg:pt-7 pb-4 lg:pb-7 my-7 mx-5 sm:mx-auto lg:mx-0 sm:w-96'>
             <Image src={samplelogo} width={200} height={200} alt='logo' />
             <p className='py-3 text-xl font-bold text-text-black'>価値観</p>
             <p className='tracking-wider leading-7 text-left text-text-black'>
               テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
             </p>
           </div>
-          <div className='hidden sm:block mx-1 w-0.5 bg-line-green'></div>
-          <div className='px-10 pt-7 mx-auto sm:mx-0 sm:w-96 max-w-md'>
-            <Image src={samplelogo} width={200} height={200} alt='logo' />
-            <p className='py-3 text-xl font-bold text-text-black'>価値観</p>
-            <p className='tracking-wider leading-7 text-left text-text-black'>
-              テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
-            </p>
-          </div>
-          <div className='hidden sm:block mx-1 w-0.5 bg-line-green'></div>
 
-          <div className='px-10 pt-7 mx-auto sm:mx-0 sm:w-96 max-w-md'>
+          <div className='hidden lg:block my-7 mx-1 w-0.5 h-96 bg-line-green'></div>
+
+          <div className=' px-4 lg:px-10 lg:pt-7 pb-4 lg:pb-7 my-7 mx-5 sm:mx-auto lg:mx-0 sm:w-96'>
+            <Image src={samplelogo} width={200} height={200} alt='logo' />
+            <p className='py-3 text-xl font-bold text-text-black'>価値観</p>
+            <p className='tracking-wider leading-7 text-left text-text-black'>
+              テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+            </p>
+          </div>
+
+          <div className='hidden lg:block my-7 mx-1 w-0.5 h-96 bg-line-green'></div>
+          <div className=' px-4 lg:px-10 lg:pt-7 pb-4 lg:pb-7 my-7 mx-5 sm:mx-auto lg:mx-0 sm:w-96 bg-white'>
             <Image src={samplelogo} width={200} height={200} alt='logo' />
             <p className='py-3 text-xl font-bold text-text-black'>価値観</p>
             <p className='tracking-wider leading-7 text-left text-text-black'>
