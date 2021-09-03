18d4baaac3006e063dcb5fa92b3dbb677cc2a23a
u-Hoshi <yuta000707@yahoo.co.jp>
2021-09-03 12:22:51 +0900

    aboutのlogoを実装

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,7 +1,28 @@
 import Image from "next/image"
-import samplelogo from "../../../public/images/about-1.png"
+import aboutlogo1 from "../../../public/images/about-1.png"
+import aboutlogo2 from "../../../public/images/about-2.png"
+import aboutlogo3 from "../../../public/images/about-3.png"
 import styles from "../../../styles/about.module.scss"
 import SectionTitle from "../datadisplay/SectionTitle"
+
+const abouts = [
+  {
+    title: "趣味1",
+    content: "hogehogehogehogehogehogehogehogehogehogehogehogehogehogehoge",
+    logo: aboutlogo1,
+  },
+  {
+    title: "趣味2",
+    content:
+      " テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト",
+    logo: aboutlogo2,
+  },
+  {
+    title: "趣味3",
+    content: " 焼肉焼肉焼肉焼肉焼肉焼肉焼肉焼肉焼肉焼肉焼肉焼肉焼肉焼肉",
+    logo: aboutlogo3,
+  },
+]
 
 const About = () => {
   return (
@@ -9,57 +30,30 @@ const About = () => {
       <div className='hidden lg:block'>
         <div className={styles.aboutpc}>
           <SectionTitle title='私について' subtitle='ABOUT' />
-          <div className='flex sm:justify-between lg:justify-between sm:px-8 md:px-12 xl:px-56 mx-auto max-w-screen-xl'>
-            <div className='pt-7 w-3/12 lg:w-72'>
-              <Image src={samplelogo} width={200} height={200} alt='logo' />
-              <p className='text-xl font-bold text-text-green'>趣味</p>
-              <p className='tracking-wider leading-7 text-left text-text-black'>
-                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
-              </p>
-            </div>
-            <div className='pt-7 w-3/12 lg:w-72'>
-              <Image src={samplelogo} width={200} height={200} alt='logo' />
-              <p className='text-xl font-bold text-text-green'>趣味</p>
-              <p className='tracking-wider leading-7 text-left text-text-black'>
-                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
-              </p>
-            </div>
-            <div className='pt-7 w-3/12 lg:w-72'>
-              <Image src={samplelogo} width={200} height={200} alt='logo' />
-              <p className='text-xl font-bold text-text-green'>趣味</p>
-              <p className='tracking-wider leading-7 text-left text-text-black'>
-                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
-              </p>
-            </div>
+          <div className='flex sm:justify-between lg:justify-between sm:px-8 md:px-24 xl:px-48 mx-auto max-w-screen-xl'>
+            {abouts.map((about: any) => (
+              <div key={about.title} className='px-2 pt-7 w-3/12 lg:w-72'>
+                <Image src={about.logo} width={200} height={200} alt='logo' />
+                <p className='text-xl font-bold text-text-green'>{about.title}</p>
+                <p className='tracking-wider leading-7 text-left text-text-black'>
+                  {about.content}
+                </p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
       {/* sp版 */}
       <div className='lg:hidden break-words'>
         <SectionTitle title='私について' subtitle='ABOUT' />
-        <div>
-          <div className='px-10 pt-7 mx-auto max-w-md'>
-            <Image src={samplelogo} width={200} height={200} alt='logo' />
-            <p className='text-xl font-bold text-text-green'>趣味</p>
-            <p className='tracking-wider leading-7 text-left text-text-black'>
-              テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
-            </p>
+
+        {abouts.map((about: any) => (
+          <div key={about.title} className='px-10 pt-7 mx-auto max-w-md'>
+            <Image src={about.logo} width={200} height={200} alt='logo' />
+            <p className='text-xl font-bold text-text-green'>{about.title}</p>
+            <p className='tracking-wider leading-7 text-left text-text-black'>{about.content}</p>
           </div>
-        </div>
-        <div className='px-10 pt-7 mx-auto max-w-md'>
-          <Image src={samplelogo} width={200} height={200} alt='logo' />
-          <p className='text-xl font-bold text-text-green'>趣味</p>
-          <p className='tracking-wider leading-7 text-left text-text-black'>
-            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
-          </p>
-        </div>
-        <div className='px-10 pt-7 mx-auto max-w-md'>
-          <Image src={samplelogo} width={200} height={200} alt='logo' />
-          <p className='text-xl font-bold text-text-green'>趣味</p>
-          <p className='tracking-wider leading-7 text-left text-text-black'>
-            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
-          </p>
-        </div>
+        ))}
       </div>
     </section>
   )
