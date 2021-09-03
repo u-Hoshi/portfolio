95468a6d9701425f24be0c8b0c876e6c621fc730
u-Hoshi <yuta000707@yahoo.co.jp>
2021-09-03 20:42:36 +0900

    valueの変更

--- src/components/datadisplay/Prpgressbar.tsx
+++ src/components/datadisplay/Prpgressbar.tsx
@@ -9,14 +9,14 @@ const Progressbar: VFC<ProgressbarType> = (props) => {
   const { content, progress } = props
   return (
     <div className='relative pt-1'>
-      <span className='inline-block py-1 px-2 text-xs font-semibold text-pink-600 uppercase'>
+      <span className='inline-block py-1 px-2 text-xs font-semibold text-progress-dark-green'>
         {content}
       </span>
       <div className='flex'>
-        <div className='flex overflow-hidden flex-1 mb-4 w-10/12 max-w-6xl h-3 text-xs bg-pink-200 rounded'>
+        <div className='flex overflow-hidden flex-1 mb-4 w-10/12 max-w-6xl h-3 text-xs bg-progress-thin-green rounded'>
           <div
             style={{ width: progress }}
-            className='flex flex-col justify-center text-center text-white whitespace-nowrap bg-pink-500 shadow-none'
+            className='flex flex-col justify-center text-center text-white whitespace-nowrap bg-progress-dark-green shadow-none'
           ></div>
         </div>
         <span className='inline-block pl-2 w-2/12 max-w-min h-3 text-xs font-semibold leading-3'>
--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -9,12 +9,12 @@ const Header = () => {
   return (
     <section id='header'>
       <div className='lg:ml-[-40px]'>
-        <div className='sm:flex justify-between mx-auto max-w-screen-xl'>
+        <div className='sm:flex justify-between mx-auto max-w-screen-2xl'>
           <div className='flex justify-center items-center lg:pl-[40px]'>
             <Image src={logo} width={150} height={75} alt='logo' />
           </div>
           <div className='sm:flex justify-center items-center lg:pl-[40px]'>
-            <div className='flex px-3 pb-2 sm:pb-0 text-center sm:text-right text-text-green'>
+            <div className='flex px-1 pb-2 sm:pb-0 sm:mr-3 text-center sm:text-right text-text-green'>
               {navs.map((nav: string) => (
                 <HeaderNav nav={nav} key={nav} />
               ))}
