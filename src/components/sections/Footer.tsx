f937e15ce7f6ef90cbea2d7215d5c6a0d4d50fa8
u-Hoshi <yuta000707@yahoo.co.jp>
2021-10-10 18:20:46 +0900

    footerの文言を削除

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -9,20 +9,10 @@ const Footer = () => {
       id='footer'
       className='relative py-11 -mb-5 text-xs text-center text-white align-middle bg-back-black'
     >
-      <div className='flex sm:hidden justify-around'>
-        <p>お問い合わせ</p>
-        <p>助成金について</p>
-      </div>
-      <div className='flex sm:hidden justify-around pb-8'>
-        <p>特定商取引法に基づく表記</p>
-        <p>プライバシーポリシー</p>
-      </div>
       <p className='hidden sm:block py-5'>
         CREATED BY <span className=' font-black'>u-Hoshi</span>
       </p>
-
-      {/* <Image className='h-60' width={50} height={60} src={icon} alt='logo' /> */}
-      <div className='flex justify-center pt-8'>
+      <div className='flex justify-center'>
         <a className='p-2 text-2xl' href='https://github.com/u-Hoshi'>
           <FaGithub />
         </a>
